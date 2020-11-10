8fac9171023db4098df4c26515376f590687059a
Martin <mselvenis@gmail.com>
2020-11-10 16:00:05 +0000

    supabse set up

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import supabase from '@/plugins/supabase'
 
 createApp(App)
+  .use(supabase)
   .use(store)
   .use(router)
-  .mount("#app");
+  .mount('#app')
